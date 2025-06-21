c086085616e6293306f5447a406d362bc3fbfa98
ctnkaan <kaan.taskingenc@gmail.com>
2025-06-21 20:06:00 +0300

    feat: none-ls format on save

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -8,6 +8,17 @@ return {
 				null_ls.builtins.formatting.prettier,
 				null_ls.builtins.diagnostics.eslint_d,
 			},
+			on_attach = function(client, bufnr)
+				if client.supports_method("textDocument/formatting") then
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						group = vim.api.nvim_create_augroup("FormatOnSave", {}),
+						buffer = bufnr,
+						callback = function()
+							vim.lsp.buf.format({ bufnr = bufnr })
+						end,
+					})
+				end
+			end,
 		})
 
 		vim.keymap.set("n", "<leader>nf", vim.lsp.buf.format, {desc = "Format with None LS"})
